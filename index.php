73e1f8a5e15e7d7a374829895b210211582dc324
Shirley940408 <Shirley940408@gmail.com>
2018-08-01 00:08:23 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once './vendor/autoload.php';
+
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader, array());
+
+echo $twig->render('index.html.twig', array('name' => 'Fabien'));
+
+
+ ?>
